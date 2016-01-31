17b9af69ad902a6ee0fb0ae6aeedf7583633f800
Dhruba <pingdjm@gmail.com>
2016-01-31 09:08:23 -0500

    Added header

--- SettingsActivity/app/src/main/java/com/pingdjmsample/settingsactivity/app/settingsactivity/SettingsActivity.java
+++ SettingsActivity/app/src/main/java/com/pingdjmsample/settingsactivity/app/settingsactivity/SettingsActivity.java
@@ -5,6 +5,8 @@ import android.os.Build;
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
 
+import java.util.List;
+
 
 /**
  * Created by dhruba on 1/30/16.
@@ -15,8 +17,21 @@ public class SettingsActivity extends PreferenceActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getFragmentManager().beginTransaction().replace(android.R.id.content,new SettingsFragment()).commit();
+
+        //getFragmentManager().beginTransaction().replace(android.R.id.content,new SettingsFragment()).commit();
         // next stage I explore the fragment based preference
 
+    }
+
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB)
+    @Override
+    public void onBuildHeaders(List<Header> target) {
+        super.onBuildHeaders(target);
+        loadHeadersFromResource(R.xml.pref_header,target);
+    }
+
+    @Override
+    protected boolean isValidFragment(String fragmentName) {
+        return true;
     }
 }
